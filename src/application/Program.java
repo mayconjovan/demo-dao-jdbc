c40ad78c513f4202d2353318c3d19fdff9b761ac
Maycon J Pereira <mayconjovan@gmail.com>
2020-03-14 14:07:45 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,16 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted, new Id = " + newSeller.getId());
+		
+		System.out.println("\n === TESTE 5: seller update ===");
+		
+		seller = sellerDao.findById(1);
+		//System.out.println(seller);
+		seller.setName("Martha Waine");
+		System.out.println(seller);
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
+		
 	}
 
 }
